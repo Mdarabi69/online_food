9a2a75a087018d15bfe67b6eacf777744f67b955
مصطفی دارابی <m.darabi@localserver.tsk>
2024-03-13 07:23:26 +0330

    init online_food project

---
+++ config/local_settings.py
@@ -0,0 +1,7 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-@qu)%(8*owrsy^lir^+=mem2^_0x*w57m*9fud#tfsnct*nj@a'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
